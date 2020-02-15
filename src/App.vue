d2248de5bea44c55ec77ff5a35fb5bc01ef9a3f9
Alan Lu <alanlu1023@gmail.com>
2020-02-15 08:28:47 -0700

    Add frontend

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="D Corp logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
